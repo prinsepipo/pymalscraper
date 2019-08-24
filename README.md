03b859b7633a3012190efcd366fd487659da9bbd
Pipo <prinsepipo.sanchez@gmail.com>
2019-08-24 19:46:18 +0800

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Anime Web Scraper
+Scrapes anime data from https://myanimelist.net/ .
+
+## Anime Model Data
+These are, as for now, the only available data.
+- Title
+- English Title
+- Japanese Title
+- Synonyms
+- Synopsis
+- Anime Type
+- Episodes
+- Genres
+- Poster
+- Trailer
+
+## Installation
+```
+pip install pymalscraper
+```
+
+## Basic Usage
+```python
+from pymalscraper.scraper import MALScraper
+scraper = MALScraper()
+data = scraper.get_anime("kimi no na wa.")
+
+> data.title
+'Kimi no na wa.'
+> data.english_title
+'Your Name.'
+> data.japanese_title
+'君の名は。'
+> data.synonyms
+''
+> data.synopsis
+"Mitsuha Miyamizu, a high school girl, yearns to live the life of a boy in the bustling city of Tokyo—a dream that stands in stark contrast..."
+> data.animetype
+'Movie'
+> data.episodes
+'1'
+> data.genres
+'Romance, Supernatural, School, Drama'
+> data.poster
+'https://cdn.myanimelist.net/images/anime/5/87048.jpg'
+> data.trailer
+'https://www.youtube.com/embed/3KR8_igDs1Y?enablejsapi=1&wmode=opaque&autoplay=1'
+
+# To get the anime url    
+> scraper.get_anime_url('kimi no na wa.')
+'https://myanimelist.net/anime/32281/Kimi_no_Na_wa'
+```
--- pymalscraper/scraper.py
+++ pymalscraper/scraper.py
@@ -7,17 +7,49 @@ import time
 
 
 class MALScraper:
-    """Scrapes https://myanimelist.net/ using BeautifulSoup4."""
+    """Scrapes https://myanimelist.net/ using BeautifulSoup4.
+
+    Methods
+    -------
+    get_anime(anime), get_anime_url(anime)
+    """
 
     def __init__(self):
         # MAL search url
         self.MAL_URL = 'https://myanimelist.net/anime.php?q='
 
     def get_anime(self, anime):
+        """Gets the anime model data.
+
+        Parameters
+        ----------
+        anime : str
+            Name of the anime.
+
+        Returns
+        -------
+        class Anime
+            Returns the scraped anime model data.
+        """
+
+        # Gets the anime url.
         anime_url = self.get_anime_url(anime)
+        #
         return Anime(anime_url)
 
     def get_anime_url(self, anime):
+        """Gets the url of the anime from the website.
+
+        Parameters
+        ----------
+        anime : str
+            Name of the anime.
+
+        Returns
+        -------
+        str
+            Returns the anime url link.
+        """
         url = self.MAL_URL + anime
 
         res = requests.get(url)
@@ -31,5 +63,5 @@ class MALScraper:
             a = soup.find('a', {'class': 'hoverinfo_trigger fw-b fl-l'})
             lnk = a['href']
         except Exception as e:
-            print('Error getting anime url.\nError: {e}')
+            print(f'Error getting anime url.\nError: {e}')
         return lnk
