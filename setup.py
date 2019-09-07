3678dd81e3742ade299a7e7e1fc73e4311a9137e
Pipo <prinsepipo.sanchez@gmail.com>
2019-09-07 19:02:01 +0800

    added dependencies to be installed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
     setup(name='pymalscraper',
-          version='1.0.2',
+          version='1.0.3',
           author='prinsepipo',
           author_email='prinse.sanchez@gmail.com',
           description='Simple Anime Web Scraper.',
@@ -14,4 +14,10 @@ with open("README.md", "r") as fh:
           url='https://github.com/prinsepipo/anime-scraper',
           license='MIT',
           packages=find_packages(),
+          install_requires=[
+              'requests',
+              'BeautifulSoup4',
+              'lxml'
+          ],
+          python_requires='>=3',
           zip_safe=False)
