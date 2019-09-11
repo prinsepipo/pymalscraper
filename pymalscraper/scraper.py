50903afba09fc1f4ee3ac1c574e7a2ff54a42060
Pipo <prinsepipo.sanchez@gmail.com>
2019-09-11 23:58:16 +0800

    added method for getting all anime.

--- pymalscraper/scraper.py
+++ pymalscraper/scraper.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 
 import time
+import sys
+from multiprocessing import Pool
 
 
 class MALScraper:
@@ -17,6 +19,9 @@ class MALScraper:
     def __init__(self):
         # MAL search url
         self.MAL_URL = 'https://myanimelist.net/anime.php?q='
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
+        }
 
     def get_anime(self, anime):
         """Gets the anime model data.
@@ -34,7 +39,7 @@ class MALScraper:
 
         # Gets the anime url.
         anime_url = self.get_anime_url(anime)
-        #
+
         return Anime(anime_url)
 
     def get_anime_url(self, anime):
@@ -51,14 +56,11 @@ class MALScraper:
             Returns the anime url link.
         """
         url = self.MAL_URL + anime
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
-        }
 
-        res = requests.get(url, headers=headers)
+        res = requests.get(url, headers=self.headers)
         while res.status_code != 200:
             time.sleep(1)
-            res = requests.get(url, headers=headers)
+            res = requests.get(url, headers=self.headers)
 
         soup = BeautifulSoup(res.text, features='lxml')
         lnk = None
@@ -68,3 +70,41 @@ class MALScraper:
         except Exception as e:
             print(f'Error getting anime url.\nError: {e}')
         return lnk
+
+    def get_all_anime(self):
+        total_anime = 16100
+        count = 0
+        url = 'https://myanimelist.net/topanime.php?limit={count}'
+        links = []
+
+        while count <= total_anime:
+            res = requests.get(url, headers=self.headers)
+
+            while res.status_code != 200:
+                time.sleep(1)
+                res = requests.get(url, headers=self.headers)
+
+            soup = BeautifulSoup(res.text, features='lxml')
+
+            try:
+                aa = soup.find_all(
+                    'a', {'class': 'hoverinfo_trigger fl-l fs14 fw-b'})
+
+                for a in aa:
+                    link = a['href']
+
+                    if link:
+                        links.append(link)
+                        print(f'Link count: {len(links)}')
+            except Exception as e:
+                print(e)
+
+            count += 50
+
+        sys.setrecursionlimit(25000)
+        p = Pool(10)
+        animes = p.map(Anime, links)
+        p.terminate()
+        p.join()
+
+        return animes
