c56bf829058b99658575c547e442f408f7e9baae
Pipo <prinsepipo.sanchez@gmail.com>
2019-10-03 01:29:51 +0800

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
     setup(name='pymalscraper',
-          version='2.1.0',
+          version='2.2.0',
           author='prinsepipo',
           author_email='prinse.sanchez@gmail.com',
           description='Simple Anime Web Scraper.',
